58c851e593319968999bebcc036bc309205b0f4d
Eva <eva.gutierrez@weewoo.com>
2021-10-27 17:20:45 +0200

    changed how to apply discounts with not register them to later make use. now at the time of receipt they are applied. and added the total function with return price total of cart

--- cash_register_master.py
+++ cash_register_master.py
@@ -7,61 +7,113 @@ import logger.logger_configurator as logger_configurator
 logger = logger_configurator.get_logger(__name__)
 
 
+def total(products):
+    """
+    This function calculates the total price to products cart
+    :param products: products cart
+    """
+
+    total_price = 0
+
+    for product in products:
+        total_price += product[const.PRODUCTS_UNITS] * product[const.PRODUCTS_PRICE]
+
+    return total_price
+
+
 def scan(code, products):
     """This function gets code to scan and add 1 unit in field units in the correspondent dictionary
     :param code: product code name
     :param products: list with exists products
     """
 
+    exist = False
+
+    # Add a unit to the product corresponding to the given code
     for p in products:
         if p[const.PRODUCTS_CODE] == code:
             p[const.PRODUCTS_UNITS] = p[const.PRODUCTS_UNITS] + 1
+            exist = True
+            logger.info("Scanned a '{}' unit".format(code))
+
+    # Not exist this product in our catalog
+    if not exist:
+        logger.error("Error scanning a '{}' unit because the product is not registered in the catalog".format(code))
 
 
-def bulk_discount(code, units, price, discounts):
+def bulk_discount(code, units, price, products):
     """This function collects the bulk discount that is requested for a specific product
     (for example, 3 or more TSHIRT items, the price per unit should be 19.00€)
     :param code: product code name
     :param units: units mark at discount
     :param price: new item price
-    :param discounts: discounts list
+    :param products: list with exists products in cart
     """
 
     # If the code is existing and the unit and price are positive values
     if len(code) > 0 and units > 0 and price > 0.0:
 
-        # created the new discount
-        dict_bulk_discount = {
-            const.PRODUCTS_CODE: code,
-            const.BULK_DISCOUNT_UNITS: units,
-            const.BULK_DISCOUNT_PRICE: price
-        }
+        exist = False
 
-        # append in discounts
-        discounts.append(dict_bulk_discount)
+        # Change price in the correcting product only the condition is true
+        for product in products:
+            if product[const.PRODUCTS_CODE] == code:
+
+                exist = True
+
+                if product[const.PRODUCTS_UNITS] >= units:
+                    product[const.PRODUCTS_PRICE] = price
+                    logger.info(
+                        "New bulk discount on '{}' items with {} units chosen or more and the price per unit be {}".
+                        format(code, units, price))
+                else:
+                    break
+
+        if not exist:
+            logger.info("Not exist '{}' product in cart".format(code, units, price))
 
 
-def group_discount(code, units_ini, units_fin, discounts):
+def group_discount(code, units_ini, units_fin, products):
     """This function collects the group discount that is requested for a specific product
     (for example, 2x1 discount on VOUCHER items)
     :param code: product code name
     :param units_ini: chosen units
     :param units_fin: units to pay
-    :param discounts: discounts list
+    :param products: list with exists products in cart
     """
 
     # If the code is existing and the units are positive values
     if len(code) > 0 and units_ini > 0 and units_fin > 0:
 
-        # created the new discount
-        dict_group_discount = {
-            const.PRODUCTS_CODE: code,
-            const.GROUP_DISCOUNT_UNITS_INI: units_ini,
-            const.GROUP_DISCOUNT_UNITS_FIN: units_fin
-        }
+        exist = False
 
-        # append in discounts
-        discounts.append(dict_group_discount)
+        # Check that the product to make this discount exists
+        for product in products:
+            if product[const.PRODUCTS_CODE] == code:
+
+                exist = True
+
+                # if have the minimum number of products to apply the discount
+                if product[const.PRODUCTS_UNITS] >= units_ini:
+
+                    # discounts 2x1, 3x1, (allx1) ...
+                    # if the number units is even
+                    if product[const.PRODUCTS_UNITS] % units_ini == 0 and units_fin == 1:
+                        product[const.PRODUCTS_UNITS] = product[const.PRODUCTS_UNITS] / units_ini
+
+                    # or the number units is odd
+                    elif product[const.PRODUCTS_UNITS] % units_ini != 0 and units_fin == 1:
+                        units = product[const.PRODUCTS_UNITS]
+                        units -= 1
+                        product[const.PRODUCTS_UNITS] = (units / units_ini) + 1
+
+                    logger.info("New group discount on '{0}' items with {1} units chosen and paid for {2} unit "
+                                "({1}x{2})".format(code, units_ini, units_fin))
+                else:
+                    break
+
+        if not exist:
+            logger.info("Not exist '{}' product in cart".format(code, units_ini, units_fin))
 
 
 def convert_products(dict_catalog):
@@ -76,7 +128,8 @@ def convert_products(dict_catalog):
 
     # Dictionary created for each product in the article catalog
     for i in range(len_row):
-        # new product
+
+        # created new product
         dict_product = {
             const.PRODUCTS_CODE: dict_catalog[const.PRODUCTS_CODE][i],
             const.PRODUCTS_NAME: dict_catalog[const.PRODUCTS_NAME][i],
@@ -104,6 +157,7 @@ def catalog_check(dict_catalog):
 
     # Or is not empty see the lengths for values
     else:
+
         # check values length
         for value in dict_catalog.values():
             len_value = len(value)
@@ -144,21 +198,168 @@ def get_products():
     # Convert to list from products
     list_products = convert_products(dict_catalog)
 
-    logger.info("Catalog: {}".format(dict_catalog))
-    logger.info("Products: {}".format(list_products))
-
-    return dict_catalog, list_products, []
+    return dict_catalog, list_products
 
 
 def main():
-    catalog, products, discounts = get_products()
+    catalog, products = get_products()
 
-    group_discount("VOUCHER", 2, 1, discounts)
-    bulk_discount("TSHIRT", 3, 19.00, discounts)
+    logger.info("Catalog: {}".format(catalog))
+    logger.info("Products: {}".format(products))
 
+    # EXERCISE CASE 1 (● Items: VOUCHER, TSHIRT, PANTS - Total: 32.50€)
+    # (with 2x1 in VOUCHER items and 3 or more TSHIRT items the price per unit should be 19.00)
+    """
+    logger.info("********************* TEST 1")
     scan("VOUCHER", products)
     scan("TSHIRT", products)
     scan("PANTS", products)
+    
+    logger.info("Products: {}".format(products))
+    group_discount("VOUCHER", 2, 1, products)
+    bulk_discount("TSHIRT", 3, 19.00, products)"""
+
+    # EXERCISE CASE 2 (● Items: VOUCHER, TSHIRT, VOUCHER - Total: 25.00€)
+    # (with 2x1 in VOUCHER items and 3 or more TSHIRT items the price per unit should be 19.00)
+    """
+    logger.info("********************* TEST 2")
+    scan("VOUCHER", products)
+    scan("TSHIRT", products)
+    scan("VOUCHER", products)
+    
+    logger.info("Products: {}".format(products))
+    group_discount("VOUCHER", 2, 1, products)
+    bulk_discount("TSHIRT", 3, 19.00, products)"""
+
+    # EXERCISE CASE 3 (● Items: TSHIRT, TSHIRT, TSHIRT, VOUCHER, TSHIRT - Total: 81.00€)
+    # (with 2x1 in VOUCHER items and 3 or more TSHIRT items the price per unit should be 19.00)
+    """
+    logger.info("********************* TEST 3")
+    scan("TSHIRT", products)
+    scan("TSHIRT", products)
+    scan("TSHIRT", products)
+    scan("VOUCHER", products)
+    scan("TSHIRT", products)
+    
+    logger.info("Products: {}".format(products))
+    group_discount("VOUCHER", 2, 1, products)
+    bulk_discount("TSHIRT", 3, 19.00, products)"""
+
+    # EXERCISE CASE 4 (● Items: VOUCHER, TSHIRT, VOUCHER, VOUCHER, PANTS, TSHIRT, TSHIRT - Total: 74.50€)
+    # (with 2x1 in VOUCHER items and 3 or more TSHIRT items the price per unit should be 19.00)
+    """
+    logger.info("********************* TEST 4")
+    scan("VOUCHER", products)
+    scan("TSHIRT", products)
+    scan("VOUCHER", products)
+    scan("VOUCHER", products)
+    scan("PANTS", products)
+    scan("TSHIRT", products)
+    scan("TSHIRT", products)
+    
+    logger.info("Products: {}".format(products))
+    group_discount("VOUCHER", 2, 1, products)
+    bulk_discount("TSHIRT", 3, 19.00, products)"""
+
+    # OWN CASE (VOUCHER, TSHIRT, VOUCHER, VOUCHER, PANTS, TSHIRT, TSHIRT, VOUCHER, VOUCHER, VOUCHER, VOUCHER: 84.50€)
+    # (with 2x1 in VOUCHER items and 3 or more TSHIRT items the price per unit should be 19.00)
+    """
+    logger.info("********************* TEST 5 (own test for 2x1 in VOUCHER items but got more VOUCHER items)")
+    scan("VOUCHER", products)
+    scan("TSHIRT", products)
+    scan("VOUCHER", products)
+    scan("VOUCHER", products)
+    scan("PANTS", products)
+    scan("TSHIRT", products)
+    scan("TSHIRT", products)
+    scan("VOUCHER", products)
+    scan("VOUCHER", products)
+    scan("VOUCHER", products)
+    scan("VOUCHER", products)
+    
+    logger.info("Products: {}".format(products))
+    group_discount("VOUCHER", 2, 1, products)
+    bulk_discount("TSHIRT", 3, 19.00, products)"""
+
+    # OWN CASE (VOUCHER, TSHIRT, VOUCHER, VOUCHER, PANTS, TSHIRT, TSHIRT, PANTS, PANTS, PANTS: 87€)
+    # (with 3x1 in PANTS items and 3 or more TSHIRT items the price per unit should be 19.00)
+    """
+    logger.info("********************* TEST 6 (own test for 3x1 in PANTS items)")
+    scan("VOUCHER", products)
+    scan("TSHIRT", products)
+    scan("VOUCHER", products)
+    scan("VOUCHER", products)
+    scan("PANTS", products)
+    scan("TSHIRT", products)
+    scan("TSHIRT", products)
+    scan("PANTS", products)
+    scan("PANTS", products)
+    scan("PANTS", products)
+
+    logger.info("Products: {}".format(products))
+    group_discount("PANTS", 3, 1, products)
+    bulk_discount("TSHIRT", 3, 19.00, products)"""
+
+    # OWN CASE (VOUCHER, TSHIRT, VOUCHER, VOUCHER, PANTS, TSHIRT, TSHIRT, PANTS, PANTS, PANTS: 79.50€)
+    # (with 4x1 in PANTS items and 3 or more TSHIRT items the price per unit should be 19.00)
+    """
+    logger.info("********************* TEST 7 (own test for 4x1 in PANTS items)")
+    scan("VOUCHER", products)
+    scan("TSHIRT", products)
+    scan("VOUCHER", products)
+    scan("VOUCHER", products)
+    scan("PANTS", products)
+    scan("TSHIRT", products)
+    scan("TSHIRT", products)
+    scan("PANTS", products)
+    scan("PANTS", products)
+    scan("PANTS", products)
+    
+    logger.info("Products: {}".format(products))
+    group_discount("PANTS", 4, 1, products)
+    bulk_discount("TSHIRT", 3, 19.00, products)"""
+
+    # Error discount (VOUCHER, TSHIRT, VOUCHER, VOUCHER, PANTS, TSHIRT, TSHIRT, PANTS, PANTS, PANTS: 102€)
+    # (with 2x1 in SHOES items and 2 or more VOUCHER items the price per unit should be 4.00)
+    """
+    logger.info("********************* TEST 8 (error case with 2x1 in SHOES items because not got SHOES items)")
+    scan("VOUCHER", products)
+    scan("TSHIRT", products)
+    scan("VOUCHER", products)
+    scan("VOUCHER", products)
+    scan("PANTS", products)
+    scan("TSHIRT", products)
+    scan("TSHIRT", products)
+    scan("PANTS", products)
+    scan("PANTS", products)
+    scan("PANTS", products)
+
+    logger.info("Products: {}".format(products))
+    group_discount("SHOES", 2, 1, products)
+    bulk_discount("VOUCHER", 2, 4.00, products)"""
+
+    # Error discount (VOUCHER, TSHIRT, VOUCHER, VOUCHER, PANTS, TSHIRT, TSHIRT, PANTS, PANTS, PANTS: 79.5€)
+    # (with 2x1 in SHOES items and 2 or more VOUCHER items the price per unit should be 4.00 and 4x1 in PANTS items)
+    """
+    logger.info("********************* TEST 9 (error case with 2x1 in SHOES items because not got SHOES items)")
+    scan("VOUCHER", products)
+    scan("TSHIRT", products)
+    scan("VOUCHER", products)
+    scan("VOUCHER", products)
+    scan("PANTS", products)
+    scan("TSHIRT", products)
+    scan("TSHIRT", products)
+    scan("PANTS", products)
+    scan("PANTS", products)
+    scan("PANTS", products)
+
+    logger.info("Products: {}".format(products))
+    group_discount("SHOES", 2, 1, products)
+    bulk_discount("VOUCHER", 2, 4.00, products)
+    group_discount("PANTS", 4, 1, products)"""
+
+    logger.info("Products: {}".format(products))
+    logger.info("Total price: {}".format(total(products)))
 
     sys.exit(const.EXIT_CODE_OK)
 
